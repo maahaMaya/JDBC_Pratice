5d9d9dd2acd9b354eb23dfe2941945f3fe8eb4a4
sourav <devbhsourav@gmail.com>
2023-05-27 15:11:39 +0530

    [sourav]add: PreparedStatement

---
+++ preparedStatement/com/jdbc/model/Student.java
@@ -0,0 +1,41 @@
+package com.jdbc.model;
+
+public class Student {
+
+	private int rollno;
+	private String stdname;
+	private String course;
+	private float fees;
+
+	public int getRollno() {
+		return rollno;
+	}
+
+	public void setRollno(int rollno) {
+		this.rollno = rollno;
+	}
+
+	public String getStdname() {
+		return stdname;
+	}
+
+	public void setStdname(String stdname) {
+		this.stdname = stdname;
+	}
+
+	public String getCourse() {
+		return course;
+	}
+
+	public void setCourse(String course) {
+		this.course = course;
+	}
+
+	public float getFees() {
+		return fees;
+	}
+
+	public void setFees(float fees) {
+		this.fees = fees;
+	}
+}
